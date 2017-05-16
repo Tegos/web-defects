ca436b1ee6340f84724e6e8a133dd4d215fb16fb
Tegos <mihavko_ivan@mail.ru>
2017-05-16 15:10:32 +0300

    Added template

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tegos on 16.05.2017.
+ */
+
+function readURL(input) {
+
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#blah').attr('src', e.target.result);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+$("#imgInp").change(function(){
+	readURL(this);
+});
