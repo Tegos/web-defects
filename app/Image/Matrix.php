c7f15ce7eb52eee66a185755c018758979502f36
Tegos <tegosiv@gmail.com>
2017-10-16 08:32:09 +0300

    Working on getGroups

--- app/Image/Matrix.php
+++ app/Image/Matrix.php
@@ -10,18 +10,16 @@ namespace App\Image;
 
 class Matrix
 {
+	const NUM_IN_GROUP = 3;
+
 	public static function findDistance($i, $j, array $dataGraphIdentification, array $data)
 	{
-
 		$sum = 0;
 		$row1 = $data[$dataGraphIdentification[$i][0]][$dataGraphIdentification[$i][1]];
 		$row2 = $data[$dataGraphIdentification[$j][0]][$dataGraphIdentification[$j][1]];
-		//$row2 = $data[$j][$i];
 		$num = count($row1);
 
 		for ($k = 0; $k < $num; $k++) {
-			//var_dump($data[$i][$k]);
-			//var_dump($data[$j][$k]);
 			$diff = $row1[$k] - $row2[$k];
 
 			$sqr = pow($diff, 2);
@@ -33,33 +31,45 @@ class Matrix
 
 	public static function getGroups($distanceMatrix)
 	{
-		$firstGroup = [];
+		$group = [];
 
 		$num = count($distanceMatrix);
 
-		$localMins = [];
-		for ($i = 0; $i < $num; $i++) {
-			$localMins[$i] = min(array_filter($distanceMatrix[$i]));
-		}
-
-		$minimum = min($localMins);
-		$graphCounter = $num;
-
-		$position = [];
-		for ($graphI = 0; $graphI < $graphCounter; $graphI++) {
-			for ($graphJ = 0; $graphJ < $graphCounter; $graphJ++) {
-				if ($distanceMatrix[$graphI][$graphJ] === $minimum) {
-					$position = [$graphI, $graphJ];
+		while (count($group) < self::NUM_IN_GROUP * 2) {
+			var_dump(count($group));
+			$localMins = [];
+			for ($i = 0; $i < $num; $i++) {
+				if (!in_array($i, $group)) {
+					$localMins[$i] = min(array_filter($distanceMatrix[$i]));
 				}
+			}
+
+			$minimum = min($localMins);
+			$graphCounter = $num;
+
+			for ($graphI = 0; $graphI < $graphCounter; $graphI++) {
+				for ($graphJ = 0; $graphJ < $graphCounter; $graphJ++) {
+					if ($distanceMatrix[$graphI][$graphJ] === $minimum) {
+						$group[] = $graphI;
+						$group[] = $graphJ;
+						break;
+					}
+				}
+			}
+
+			$group = array_unique($group);
+			if (count($group) > self::NUM_IN_GROUP) {
+				//$group = array_slice($group, 0, self::NUM_IN_GROUP);
 			}
 		}
 
 
-
 		//dd($minimum);
+		dd($group);
 		//dd($position);
 		//dd($localMins);
 		//dd($distanceMatrix);
+		return $group;
 
 	}
 
